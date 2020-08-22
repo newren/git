b894142efad40f0cfab9f481167b8d5ce69e2226
Elijah Newren <newren@gmail.com>
2020-08-22 10:23:06 -0700

    Revert "rev->commits valgrind stuff that I'm still unsure about"
    This reverts commit 3139c0705f01a577d8323f3f9918b7049460200e.

--- revision.c
+++ revision.c
@@ -4186,8 +4186,6 @@ struct commit *get_revision(struct rev_info *revs)
 			free_commit_list(revs->previous_parents);
 			revs->previous_parents = NULL;
 		}
-		free_commit_list(revs->commits);
-		revs->commits = NULL;
 	}
 	return c;
 }
