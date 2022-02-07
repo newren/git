de0b611f5cf94de65f7bfad2d605d7d6a372413e
Elijah Newren <newren@gmail.com>
2022-02-07 09:57:08 -0800

    Allow create_commit to take a variable number of arguments

--- builtin/replay.c
+++ builtin/replay.c
@@ -50,11 +50,13 @@ static char *get_author(const char *message)
 static struct commit *create_commit(struct repository *repo,
 				    struct tree *tree,
 				    struct commit *based_on,
-				    struct commit *parent)
+				    ... /* a bunch of struct commit * parent */)
 {
+	va_list parent_list;
+	struct commit *parent;
 	struct object_id ret;
 	struct object *obj = NULL;
-	struct commit_list *parents = NULL;
+	struct commit_list *parents = NULL, **tmp = &parents;
 	char *author;
 	char *sign_commit = NULL; /* FIXME: cli users might want to sign again */
 	struct commit_extra_header *extra = NULL;
@@ -65,7 +67,11 @@ static struct commit *create_commit(struct repository *repo,
 	const char *orig_message = NULL;
 	const char *exclude_gpgsig[] = { "gpgsig", NULL };
 
-	commit_list_insert(parent, &parents);
+	va_start(parent_list, based_on);
+	while ((parent = va_arg(parent_list, struct commit *)))
+		tmp = commit_list_append(parent, tmp);
+	va_end(parent_list);
+
 	extra = read_commit_extra_headers(based_on, exclude_gpgsig);
 	find_commit_subject(message, &orig_message);
 	strbuf_addstr(&msg, orig_message);
@@ -287,7 +293,8 @@ static struct commit *pick_regular_commit(struct repository *repo,
 	strbuf_release(&parent2_desc);
 	if (!result->clean)
 		return NULL;
-	return create_commit(repo, result->tree, pickme, replayed_base);
+
+	return create_commit(repo, result->tree, pickme, replayed_base, NULL);
 }
 
 static int add_ref_to_transaction(struct ref_transaction *transaction,
